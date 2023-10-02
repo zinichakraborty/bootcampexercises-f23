26265b42d3976f53d4a9d54bfcb4fd89c704db4e
Long Lam <lamhoanglong.3001@gmail.com>
2023-10-02 16:47:41 -0400

    feat: restructure project

---
+++ exer5/marta-interface/src/server/station.js
@@ -0,0 +1,16 @@
+// Stations by Line
+
+const stationsByLine = {
+    red: ["North Springs", "Sandy Springs", "Dunwoody", "Medical Center", "Buckhead", "Lindbergh Center", "Arts Center", "Midtown", "North Avenue", "Civic Center", "Peachtree Center", "Five Points", "Garnett", "West End", "Oakland City", "Lakewood/Ft. McPherson", "East Point", "College Park", "Airport"],
+    gold: ["Doraville", "Chamblee", "Brookhaven", "Lenox", "Lindbergh Center", "Arts Center", "Midtown", "North Avenue", "Civic Center", "Peachtree Center", "Five Points", "Garnett", "West End", "Oakland City", "Lakewood/Ft. McPherson", "East Point", "College Park", "Airport"],
+    blue: ["Hamilton E. Holmes", "West Lake", "Ashby", "Vine City", "GWCC/CNN Center", "Five Points", "Georgia State", "King Memorial", "Inman Park", "Edgewood", "East Lake", "Decatur", "Avondale", "Kensington", "Indian Creek"],
+    green: ["Bankhead", "Ashby", "Vine City", "GWCC/CNN Center", "Five Points", "Georgia State", "King Memorial", "Inman Park", "Edgewood"]
+
+}
+
+export default function getStationsByLine(line) {
+    return stationsByLine[line.toLowerCase()];
+}
+
+//Testing
+console.log(getStationsByLine("green"));
