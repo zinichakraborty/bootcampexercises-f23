e2fdecc7ea905c3000af2caa84a065a790c5b9cb
Long Lam <lamhoanglong.3001@gmail.com>
2023-10-04 00:07:37 -0400

    feat: add express backend and modify npm start to run both react and express

--- exer5/marta-interface/src/App.js
+++ exer5/marta-interface/src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Train from './components/Train'
+import useSWR from 'swr'
+import axios from 'axios'
+
+const fetcher = async (url) => {
+  const res = await axios.get(url)
+  return res.data
+}
 
 function App() {
+  const { data, error, isLoading, isValidating } = useSWR("http://localhost:3001/arrivals/gold", fetcher)
+  console.log(data)
   return (
     <div className="App">
-      
+      <p>Gold</p>
     </div>
   );
 }
--- exer5/marta-interface/src/server/railArrival.js
+++ exer5/marta-interface/src/server/railArrival.js
@@ -1,7 +1,27 @@
+import express from "express";
 import axios from "axios";
 import https from "https";
+import cors from "cors"
 
-export default async function getTrainByLine(line) {
+const server = express();
+const PORT = 3001;
+
+// Enable CORS
+server.use(cors())
+
+server.listen(PORT, (err) => {
+    if (err) {
+        console.log(err)
+    }
+    console.log(`Server running on http://localhost:${PORT}/`)
+});
+
+server.get('/arrivals/gold', async (req, res) => {
+    let arrival = await getTrainByLine("gold");
+    res.json(arrival);
+})
+
+async function getTrainByLine(line) {
     let requestSettings = {
         method: "get",
         url: "https://developerservices.itsmarta.com:18096/railrealtimearrivals?apiKey=aa09b111-e99b-4a7c-83f7-8cb6ce8857f7",
@@ -10,9 +30,5 @@ export default async function getTrainByLine(line) {
         }),
     };
     let res = await axios(requestSettings);
-
     return res.data.RailArrivals.filter(arrival => { return arrival.LINE == line.toUpperCase() });
 }
-
-//Testing
-console.log(await getTrainByLine("gold"));
