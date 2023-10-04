d8c3d70ef859f717bcc8e3617b3f58c9341254c7
llam36 <llam36@gatech.edu>
2023-10-04 13:09:14 -0400

    feat: display api res

--- exer5/marta-interface/src/App.js
+++ exer5/marta-interface/src/App.js
@@ -11,12 +11,15 @@ const fetcher = async (url) => {
 
 function App() {
   const { data, error, isLoading, isValidating } = useSWR("http://localhost:3001/arrivals/gold", fetcher)
-  console.log(data)
-  return (
-    <div className="App">
-      <p>Gold</p>
-    </div>
-  );
+  if (isLoading) return <div>Loading</div>
+  if (!isValidating) {
+    console.log(data)
+    return (
+      <div className="App">
+        {data[0].DESTINATION}
+      </div>
+    );
+  }
 }
 
 export default App;
--- exer5/marta-interface/src/server/railArrival.js
+++ exer5/marta-interface/src/server/railArrival.js
@@ -16,10 +16,16 @@ server.listen(PORT, (err) => {
     console.log(`Server running on http://localhost:${PORT}/`)
 });
 
-server.get('/arrivals/gold', async (req, res) => {
-    let arrival = await getTrainByLine("gold");
+server.get('/arrivals/:line', async (req, res) => {
+    let arrival = await getTrainByLine(req.params.line);
     res.json(arrival);
 })
+
+server.get('/stations/:line', (req, res) => {
+    res.json(getStationsByLine(req.params.line));
+})
+
+//=== UTIL ===
 
 async function getTrainByLine(line) {
     let requestSettings = {
@@ -31,4 +37,16 @@ async function getTrainByLine(line) {
     };
     let res = await axios(requestSettings);
     return res.data.RailArrivals.filter(arrival => { return arrival.LINE == line.toUpperCase() });
+}
+
+function getStationsByLine(line) {
+    const stationsByLine = {
+        red: ["North Springs", "Sandy Springs", "Dunwoody", "Medical Center", "Buckhead", "Lindbergh Center", "Arts Center", "Midtown", "North Avenue", "Civic Center", "Peachtree Center", "Five Points", "Garnett", "West End", "Oakland City", "Lakewood/Ft. McPherson", "East Point", "College Park", "Airport"],
+        gold: ["Doraville", "Chamblee", "Brookhaven", "Lenox", "Lindbergh Center", "Arts Center", "Midtown", "North Avenue", "Civic Center", "Peachtree Center", "Five Points", "Garnett", "West End", "Oakland City", "Lakewood/Ft. McPherson", "East Point", "College Park", "Airport"],
+        blue: ["Hamilton E. Holmes", "West Lake", "Ashby", "Vine City", "GWCC/CNN Center", "Five Points", "Georgia State", "King Memorial", "Inman Park", "Edgewood", "East Lake", "Decatur", "Avondale", "Kensington", "Indian Creek"],
+        green: ["Bankhead", "Ashby", "Vine City", "GWCC/CNN Center", "Five Points", "Georgia State", "King Memorial", "Inman Park", "Edgewood"]
+
+    }
+    const output = line.toLowerCase() in stationsByLine ? stationsByLine[line.toLowerCase()] : [];
+    return output;
 }
