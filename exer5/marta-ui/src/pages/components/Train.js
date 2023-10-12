8de90578ea40683dea5e32d59bba5580f87539a0
zchakraborty <113480497+zinichakraborty@users.noreply.github.com>
2023-10-11 21:19:13 -0400

    Train component

--- exer5/marta-ui/src/pages/components/Train.js
+++ exer5/marta-ui/src/pages/components/Train.js
@@ -1,10 +1,20 @@
 import "./styles/train.css"
+import m from './styles/m.png'
 
 export default function Train(props) {
-    const {DESTINATION, STATION, WAITING_TIME,  DELAY} = props;
+    const {DESTINATION, LINE, STATION, WAITING_TIME,  DELAY} = props;
+    const time = (DELAY === 'T0S') ? "On Time" : "Delayed";
     return (
         <div className="Train">
-            <p>{DESTINATION}, {STATION}, {WAITING_TIME}, {DELAY}</p>
+            <img src={m} />
+            <div className="middle">
+                <p1>{STATION} to {DESTINATION}</p1>
+                <div className="bottom-middle">
+                    <p3>{LINE}</p3>
+                    <p4>{WAITING_TIME}</p4>
+                </div>
+            </div>
+             <p2>{time}</p2>
         </div>
     )
 }
