947eaecad38942ff055460a1f010c5bfd36b8a07
zchakraborty <113480497+zinichakraborty@users.noreply.github.com>
2023-10-12 18:50:42 -0400

    add state

--- exer5/marta-ui/src/App.js
+++ exer5/marta-ui/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import TrainLine from './pages/trainLine'
+import React, { useState } from 'react';
 
 function App() {
+  const [line, setLine] = useState("gold");
+  const [buttons, setButtons] = useState(["Arriving", "Scheduled", "Northbound", "Southbound"]);
   return (
     <div className="App">
-      <TrainLine line={"gold"} buttons={["Arriving", "Scheduled", "Northbound", "Southbound"]} />
-      <TrainLine line={"red"} buttons={["Arriving", "Scheduled", "Northbound", "Southbound"]} />
-      <TrainLine line={"blue"} buttons={["Arriving", "Scheduled", "Eastbound", "Westbound"]} />
-      <TrainLine line={"green"} buttons={["Arriving", "Scheduled", "Eastbound", "Westbound"]} />
+      <button className="gold-button" onClick={() => {setLine("gold"); setButtons(["Arriving", "Scheduled", "Northbound", "Southbound"])}}>Gold</button>
+      <button className="red-button" onClick={() => {setLine("red"); setButtons(["Arriving", "Scheduled", "Northbound", "Southbound"])}}>Red</button>
+      <button className="blue-button" onClick={() => {setLine("blue"); setButtons(["Arriving", "Scheduled", "Eastbound", "Westbound"])}}>Blue</button>
+      <button className="green-button" onClick={() => {setLine("green"); setButtons(["Arriving", "Scheduled", "Eastbound", "Westbound"])}}>Green</button>
+      <TrainLine line={line} buttons={buttons} />
     </div>
   );
 }
